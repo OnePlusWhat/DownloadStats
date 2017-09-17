44c8ec2caf2a202e58058a76c72bc1fba9e56854
Harsh Shandilya <msfjarvis@gmail.com>
2017-09-17 23:26:13 +0530

    DownloadStats: Reformat
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- stats.py
+++ stats.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
- 
+
 import sys
- 
-if (len(sys.argv) < 2):
-    print("Usage: " + sys.argv[0] + " github-user [github-project]")
-    exit(1)
- 
+import io
+import json
+
 try:
   import requests
 except ImportError:
@@ -13,10 +11,11 @@ except ImportError:
     print("Installing Requests is simple with pip:\n  pip install requests")
     print("More info: http://docs.python-requests.org/en/latest/")
     exit(1)
-import io
-import json
- 
- 
+
+if (len(sys.argv) < 2):
+    print("Usage: " + sys.argv[0] + " github-user [github-project]")
+    exit(1)
+
 def dict_to_object(d):
     if '__class__' in d:
         class_name = d.pop('__class__')
@@ -28,32 +27,30 @@ def dict_to_object(d):
     else:
         inst = d
     return inst
- 
- 
+
 def ensure_str(s):
     if isinstance(s, unicode):
         s = s.encode('utf-8')
     return s
- 
+
 full_names = []
- 
+
 if len(sys.argv) == 3:
     full_names.append(sys.argv[1] + "/" + sys.argv[2])
 else:
     buf = io.StringIO()
     r = requests.get('https://api.github.com/users/' + sys.argv[1] + '/repos')
     myobj = r.json()
- 
+
     for rep in myobj:
         full_names.insert(0, ensure_str(rep['full_name']))
- 
- 
+
 for full_name in full_names:
     buf = io.StringIO()
     try:
         r = requests.get('https://api.github.com/repos/' + full_name + '/releases')
         myobj = r.json()
- 
+
         for p in myobj:
             if "assets" in p:
                 for asset in p['assets']:
