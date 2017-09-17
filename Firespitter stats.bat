0eea3f404556471f8e8f27a0a06f05550788ea01
Harsh Shandilya <msfjarvis@gmail.com>
2017-09-17 23:25:00 +0530

    DownloadStats: Nuke windows stuff
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- Firespitter stats.bat
+++
@@ -1,2 +0,0 @@
-c:\Python34\python.exe stats.py Snjo Firespitter
-pause
--- stats.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-@echo Enter github user and repo name to fetch download stats
-set /P name=Github username: %=%
-set /P repo=repo: %=%
-c:\Python34\python.exe stats.py %name% %repo%
-pause
