3a4a223d122f0a0f47a4bc343e114cae5e8abbaf
Harsh Shandilya <msfjarvis@gmail.com>
2017-09-19 00:01:38 +0530

    DownloadStats: Address pylint and PEP8 issues
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- stats.py
+++ stats.py
@@ -2,42 +2,44 @@
 
 import sys
 import io
-import json
+from future.types.newstr import unicode
 
 try:
-  import requests
+    import requests
 except ImportError:
     print("Error: requests is not installed")
     print("Run pip install -r requirements.txt before continuing")
     exit(1)
 
-if (len(sys.argv) < 2):
+if len(sys.argv) < 2:
     print("Usage: " + sys.argv[0] + " github-user [github-project]")
     exit(1)
+
 
 def dict_to_object(d):
     if '__class__' in d:
         class_name = d.pop('__class__')
         module_name = d.pop('__module__')
-        module = __import__(module_name)
-        class_ = getattr(module, class_name)
+        module_name = __import__(module_name)
+        class_ = getattr(module_name, class_name)
         args = dict((key.encode('ascii'), value) for key, value in d.items())
         inst = class_(**args)
     else:
         inst = d
     return inst
 
+
 def ensure_str(s):
     if isinstance(s, unicode):
         s = s.encode('utf-8')
     return s
+
 
 full_names = []
 
 if len(sys.argv) == 3:
     full_names.append(sys.argv[1] + "/" + sys.argv[2])
 else:
-    buf = io.StringIO()
     r = requests.get('https://api.github.com/users/' + sys.argv[1] + '/repos')
     myobj = r.json()
 
