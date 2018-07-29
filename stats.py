23696bf22828fba91aac1d51a9333944f0474f56
Harsh Shandilya <harsh@prjkt.io>
2018-07-29 17:11:12 +0530

    DownloadStats: Cleanup pylint3 issues
    Disable false positive E1102 not-callable
    https://github.com/PyCQA/pylint/issues/1493
    https://github.com/PyCQA/pylint/issues/1460
    https://www.logilab.org/ticket/4294
    
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- stats.py
+++ stats.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
-import sys
+# pylint: disable=not-callable
+
+"""
+DownloadStats: Pull statistics about download counts of GitHub releases
+of specified projects
+"""
 import io
+import sys
 
 from future.types.newstr import unicode
 
-tag_name = ""
 try:
     import requests
 except ImportError:
@@ -13,48 +18,52 @@ except ImportError:
     print("Run pip install -r requirements.txt before continuing")
     exit(1)
 
+TAG_NAME = ""
 
-def ensure_str(s):
-    if isinstance(s, unicode):
-        s = s.encode('utf-8')
-    return s
+def ensure_str(string):
+    """
+    ensure_str: Ensure strings are UTF-8 encoded
+    """
+    if isinstance(string, unicode):
+        string = string.encode('utf-8')
+    return string
 
 
-full_names = []
+FULL_NAMES = []
 
 if len(sys.argv) == 3:
-    full_names.append(sys.argv[1] + "/" + sys.argv[2])
+    FULL_NAMES.append(sys.argv[1] + "/" + sys.argv[2])
 elif len(sys.argv) == 2:
-    r = requests.get('https://api.github.com/users/' + sys.argv[1] + '/repos')
-    myobj = r.json()
+    API_RESPONSE = requests.get('https://api.github.com/users/' + sys.argv[1] + '/repos').json()
 
-    for rep in myobj:
-        full_names.insert(0, ensure_str(rep['full_name']))
+    for rep in API_RESPONSE:
+        FULL_NAMES.insert(0, ensure_str(rep['full_name']))
 elif len(sys.argv) == 4:
-    full_names.append(sys.argv[1] + "/" + sys.argv[2])
-    tag_name = sys.argv[3]
+    FULL_NAMES.append(sys.argv[1] + "/" + sys.argv[2])
+    TAG_NAME = sys.argv[3]
 else:
     print("Usage: " + sys.argv[0] + " github-user [github-project]")
     exit(1)
 
-for full_name in full_names:
+for full_name in FULL_NAMES:
     buf = io.StringIO()
     try:
-        r = requests.get('https://api.github.com/repos/' + full_name + '/releases')
-        myobj = r.json()
+        API_RESPONSE = requests.get('https://api.github.com/repos/' + full_name + '/releases').json()
 
-        for p in myobj:
+        for p in API_RESPONSE:
             print("\n")
             if "assets" in p:
                 print("Tag: " + p['tag_name'] + ", Created on " + p['created_at'].split('T')[0])
                 for asset in p['assets']:
-                    if len(tag_name) == 0 or p['tag_name'] != tag_name:
-                        print(asset['name'] + ", Downloaded " + str(asset['download_count']) + " times")
+                    if len(TAG_NAME) == 0 or p['tag_name'] != TAG_NAME:
+                        print(asset['name'] + ", Downloaded "
+                              + str(asset['download_count']) + " times")
                     else:
-                        if p['tag_name'] == tag_name:
-                            print(asset['name'] + ", Downloaded " + str(asset['download_count']) + " times")
+                        if p['tag_name'] == TAG_NAME:
+                            print(asset['name'] + ", Downloaded "
+                                  + str(asset['download_count']) + " times")
                             exit(2)
             else:
                 print("No data")
-    except:
+    except KeyError:
         pass
